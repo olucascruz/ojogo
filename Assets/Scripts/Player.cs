689ace2ae48c93a8225f1990423e8f49e84a2166
LucasCruz <lca.snf21@uea.edu.br>
2022-05-09 14:49:57 -0400

    update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -27,18 +27,21 @@ public class Player : MonoBehaviour
 
     void Move()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * Speed;
+        //Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        //transform.position += movement * Time.deltaTime * Speed;
 
-        if(Input.GetAxis("Horizontal")>0f){
+        float movement = Input.GetAxis("Horizontal");
+        rig.velocity = new Vector2(movement * Speed, rig.velocity.y);
+
+        if(movement > 0f){
         anim.SetBool("run", true);
         transform.eulerAngles = new Vector3(0f, 0f, 0f);
         }
-        if(Input.GetAxis("Horizontal")<0f){
+        if(movement < 0f){
         anim.SetBool("run", true);
         transform.eulerAngles = new Vector3(0f, 180f, 0f);
         }
-        if(Input.GetAxis("Horizontal") == 0f){
+        if(movement == 0f){
         anim.SetBool("run", false);
         }
     }
@@ -57,7 +60,7 @@ public class Player : MonoBehaviour
         }else{
             if(doubleJump)
             {
-                rig.AddForce(new Vector2(0f, JumpForce*1.5f), ForceMode2D.Impulse);
+                rig.AddForce(new Vector2(0f, JumpForce*1.2f), ForceMode2D.Impulse);
                 doubleJump = false;
             }
         }
